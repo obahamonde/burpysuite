94af39ff9a673e6e1a0ee41c3231057d837140e9
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-04-17 17:36:43 -0500

    fixed fe

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue'
+
+const { isAuthenticated, loginWithRedirect, logout, user } = useAuth0()
+
+
 useTitle('Burpy Suite')
 const { state, notify } = useStore()
 const domainRegexp = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/
@@ -35,37 +40,46 @@ for (const record of response.value.records) {
 <button title="Bur Py Suite">
 <img src="/favicon.png" x8 hover:cp  alt="logo" class="w-1/2" />
 </button>
-<input type="text" v-model="domain" class="input"
-@keyup.enter="checkDomain()" />
-<div v-if="state.isFetching" col start>
-<Loading />
-</div>
-<section gap-2 v-else-if="response" col center>
-<br />
-    <h1 text-xl font-sans underline><strong>Geolocation</strong></h1>
-<p text-body><strong>City:</strong> {{ response.geo.city }}</p>
-<p text-body><strong>Country:</strong> {{ response.geo.country }}</p>
-<p text-body><strong>AS:</strong> {{ response.geo.org.split(' ')[0] }}</p>
-<p text-body><strong>ISP:</strong> {{ response.geo.org.split(' ').slice(1).join(' ') }}</p>
-<p text-body><strong>Postal Code:</strong> {{ response.geo.postal }}</p>
-<p text-body><strong>Region:</strong> {{ response.geo.region }}</p>
-<p text-body><strong>Timezone:</strong> {{ response.geo.timezone }}</p>
-<iframe sh rounded m-4 width="500" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" :src="'https://maps.google.com/maps?ll='+response.geo.loc+'&amp;z=16&amp;output=embed'"></iframe>
-<br />
-<h1 text-xl font-sans underline><strong>HTTP Headers</strong></h1>
-<code  text-caption w-full p-8 >
-<pre lang="json"  class="bg-black p-12 w-1/2 col center mx-auto overflow-x-scroll font-mono text-success"  >{{ response.headers }}</pre>
-<br />
-</code>
-<h1 text-xl font-sans underline><strong>DNS Records</strong></h1>
-<div class="grid grid-cols-3 gap-4 mb-8">
-    <div class="v-card" v-for="record in records" :key="record.domain">
-<p class="v-card-text"><strong>Domain:</strong> {{ record.domain }}</p>
-<p class="v-card-text"><strong>Type:</strong> {{ record.type_ }}</p>
-<p class="v-card-text"><strong>Value:</strong> {{ record.value }}</p>
-</div>
-</div>
-</section>
+            <div v-if="isAuthenticated" col center>
+            <input type="text" v-model="domain" class="input"
+            @keyup.enter="checkDomain()" />
+            <div v-if="state.isFetching" col start>
+            <Loading />
+            </div>
+                <section gap-2 v-if="response" col center>
+                <br />
+                    <h1 text-xl font-sans underline><strong>Geolocation</strong></h1>
+                <p text-body><strong>City:</strong> {{ response.geo.city }}</p>
+                <p text-body><strong>Country:</strong> {{ response.geo.country }}</p>
+                <p text-body><strong>AS:</strong> {{ response.geo.org.split(' ')[0] }}</p>
+                <p text-body><strong>ISP:</strong> {{ response.geo.org.split(' ').slice(1).join(' ') }}</p>
+                <p text-body><strong>Postal Code:</strong> {{ response.geo.postal }}</p>
+                <p text-body><strong>Region:</strong> {{ response.geo.region }}</p>
+                <p text-body><strong>Timezone:</strong> {{ response.geo.timezone }}</p>
+                <iframe sh rounded m-4 width="500" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" :src="'https://maps.google.com/maps?ll=' + response.geo.loc + '&amp;z=16&amp;output=embed'"></iframe>
+                <br />
+                <h1 text-xl font-sans underline><strong>HTTP Headers</strong></h1>
+                <code  text-caption w-full p-8 >
+                <pre lang="json"  class="bg-black p-12 w-1/2 col center mx-auto overflow-x-scroll font-mono text-success"  >{{ response.headers }}</pre>
+                <br />
+                </code>
+                <h1 text-xl font-sans underline><strong>DNS Records</strong></h1>
+                <div class="grid grid-cols-3 gap-4 mb-8">
+                    <div class="v-card" v-for="record in records" :key="record.domain">
+                <p class="v-card-text"><strong>Domain:</strong> {{ record.domain }}</p>
+                <p class="v-card-text"><strong>Type:</strong> {{ record.type_ }}</p>
+                <p class="v-card-text"><strong>Value:</strong> {{ record.value }}</p>
+                </div>
+                </div>
+                </section>
+
+                <div v-else col center>
+                    Enter a domain to get started
+                </div>
+            </div>
+            <div v-else col center>
+                <button @click="login()" class="btn-get">Login</button>
+            </div>
 </section>
 </template>
 <style global>
