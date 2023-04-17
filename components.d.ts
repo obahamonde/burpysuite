a31439e0f672a6cde6ec6bc4ff548a7f22d5eb7f
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-04-17 17:09:30 -0500

    cicd

--- components.d.ts
+++ components.d.ts
@@ -12,8 +12,5 @@ declare module '@vue/runtime-core' {
     Notifier: typeof import('./src/components/Notifier.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheCounter: typeof import('./src/components/TheCounter.vue')['default']
-    TheFooter: typeof import('./src/components/TheFooter.vue')['default']
-    TheInput: typeof import('./src/components/TheInput.vue')['default']
   }
 }
